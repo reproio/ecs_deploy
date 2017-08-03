294f7bedb12fd5088e7864e1812c721a6d943ab2
joker1007 <kakyoin.hierophant@gmail.com>
2017-08-03 19:51:56 +0900

    Remove execution feature

--- lib/ecs_deploy/task_definition.rb
+++ lib/ecs_deploy/task_definition.rb
@@ -56,41 +56,5 @@ module EcsDeploy
       EcsDeploy.logger.info "register task definition [#{@task_definition_name}] [#{@region}] [#{Paint['OK', :green]}]"
       res.task_definition
     end
-
-    def run(info)
-      resp = @client.run_task({
-        cluster: info[:cluster],
-        task_definition: @task_definition_name,
-        overrides: {
-          container_overrides: info[:container_overrides] || []
-        },
-        count: info[:count] || 1,
-        started_by: "capistrano",
-      })
-      unless resp.failures.empty?
-        resp.failures.each do |f|
-          raise "#{f.arn}: #{f.reason}"
-        end
-      end
-
-      wait_targets = Array(info[:wait_stop])
-      unless wait_targets.empty?
-        @client.wait_until(:tasks_running, cluster: info[:cluster], tasks: resp.tasks.map { |t| t.task_arn })
-        @client.wait_until(:tasks_stopped, cluster: info[:cluster], tasks: resp.tasks.map { |t| t.task_arn })
-
-        resp = @client.describe_tasks(cluster: info[:cluster], tasks: resp.tasks.map { |t| t.task_arn })
-        resp.tasks.each do |t|
-          t.containers.each do |c|
-            next unless wait_targets.include?(c.name)
-
-            unless c.exit_code.zero?
-              raise "Task has errors: #{c.reason}"
-            end
-          end
-        end
-      end
-
-      EcsDeploy.logger.info "run task [#{@task_definition_name} #{info.inspect}] [#{@region}] [#{Paint['OK', :green]}]"
-    end
   end
 end
