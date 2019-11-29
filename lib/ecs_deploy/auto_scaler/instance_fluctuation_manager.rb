75e1b2edcc90beb79904979ef44daad247258a57
Kenji Okimoto <kenji.okimoto@repro.io>
2019-11-29 17:39:21 +0900

    Add EcsDeploy::AutoScaler::InstanceFluctuationManager
    This class manages increasing/decreasing EC2 instances for ECS cluster.

---
+++ lib/ecs_deploy/auto_scaler/instance_fluctuation_manager.rb
@@ -0,0 +1,165 @@
+require "aws-sdk-autoscaling"
+require "aws-sdk-ec2"
+require "aws-sdk-ecs"
+
+module EcsDeploy
+  module AutoScaler
+    class InstanceFluctuationManager
+      attr_reader :logger
+
+      def initialize(region:, cluster:, cluster_to_asg:, desired_capacity:, logger: nil)
+        @region = region
+        @cluster = cluster
+        @cluster_to_asg = cluster_to_asg
+        @desired_capacity = desired_capacity
+        @logger = logger
+
+        unless cluster_to_asg.key?(cluster)
+          raise ArgumentError, "Unknown cluster: #{cluster}"
+        end
+
+        @asg_name = cluster_to_asg[cluster]
+      end
+
+      def increase
+        asg = as_client.describe_auto_scaling_groups(auto_scaling_group_names: [@asg_name]).auto_scaling_groups.first
+
+        @logger&.info("Increase desired capacity of #{@asg_name}: #{asg.desired_capacity} => #{asg.max_size}")
+        as_client.update_auto_scaling_group(auto_scaling_group_name: @asg_name, desired_capacity: asg.max_size)
+
+        # Run in background because increasing instances may take time
+        Thread.new do
+          loop do
+            cluster = ecs_client.describe_clusters(clusters: [@cluster]).clusters.first
+            instance_count = cluster.registered_container_instances_count
+            if instance_count == asg.max_size
+              @logger&.info("Succeeded to increase instance count!")
+              break
+            end
+            @logger&.info("Current registered instance count: #{instance_count}")
+            sleep 5
+          end
+        end
+      end
+
+      def decrease
+        asg = as_client.describe_auto_scaling_groups(auto_scaling_group_names: [@asg_name]).auto_scaling_groups.first
+
+        decrease_count = asg.desired_capacity - @desired_capacity
+        if decrease_count <= 0
+          @logger&.info("The capacity is already #{asg.desired_capacity}")
+          return
+        end
+        @logger&.info("Decrease desired capacity of #{@asg_name}: #{asg.desired_capacity} => #{@desired_capacity}")
+
+        container_instance_arns = ecs_client.list_container_instances(
+          cluster: @cluster
+        ).container_instance_arns
+        container_instances = ecs_client.describe_container_instances(
+          cluster: @cluster,
+          container_instances: container_instance_arns
+        ).container_instances
+
+        region_to_container_instances = container_instances.sort_by {|ci| - ci.running_tasks_count }.group_by do |ci|
+          ci.attributes.find {|attribute| attribute.name == "ecs.availability-zone" }.value
+        end
+        target_container_instances = if region_to_container_instances.empty?
+                                       []
+                                     else
+                                       decrease_count.times.map do |i|
+                                         region = region_to_container_instances.keys[i % region_to_container_instances.size]
+                                         region_to_container_instances[region].pop
+                                       end
+                                     end
+
+        threads = []
+        target_container_instances.map(&:container_instance_arn).each_slice(10) do |arns|
+          ecs_client.update_container_instances_state(
+            cluster: @cluster,
+            container_instances: arns,
+            status: "DRAINING"
+          )
+          arns.each do |arn|
+            threads << Thread.new(arn) do |a|
+              stop_tasks(a)
+            end
+          end
+        end
+
+        threads.each(&:join)
+
+        instance_ids = target_container_instances.map(&:ec2_instance_id)
+        terminate_instances(instance_ids)
+        @logger&.info("Succeeded to decrease instances!")
+      end
+
+      private
+
+      def as_client
+        @as_client ||= Aws::AutoScaling::Client.new(
+          access_key_id: EcsDeploy.config.access_key_id,
+          secret_access_key: EcsDeploy.config.secret_access_key,
+          region: @region,
+          logger: @logger
+        )
+      end
+
+      def ec2_client
+        @ec2_client ||= Aws::EC2::Client.new(
+          access_key_id: EcsDeploy.config.access_key_id,
+          secret_access_key: EcsDeploy.config.secret_access_key,
+          region: @region,
+          logger: @logger
+        )
+      end
+
+      def ecs_client
+        @ecs_client ||= Aws::ECS::Client.new(
+          access_key_id: EcsDeploy.config.access_key_id,
+          secret_access_key: EcsDeploy.config.secret_access_key,
+          region: @region,
+          logger: @logger
+        )
+      end
+
+      def stop_tasks(arn)
+        running_task_arns = ecs_client.list_tasks(cluster: @cluster, container_instance: arn).task_arns
+        unless running_task_arns.empty?
+          running_tasks = ecs_client.describe_tasks(cluster: @cluster, tasks: running_task_arns).tasks
+          running_tasks.each do |task|
+            ecs_client.stop_task(cluster: @cluster, task: task.task_arn) if task.group.start_with?("family:")
+          end
+          ecs_client.wait_until(:tasks_stopped, cluster: @cluster, tasks: running_task_arns)
+        end
+        @logger&.info("Task #{arn.split('/').last} stopped")
+      end
+
+      def terminate_instances(instance_ids)
+        if instance_ids.empty?
+          @logger&.info("There are no instances to terminate.")
+          return
+        end
+        instance_ids.each_slice(20) do |ids|
+          as_client.detach_instances(
+            auto_scaling_group_name: @asg_name,
+            instance_ids: ids,
+            should_decrement_desired_capacity: true
+          )
+        end
+
+        ec2_client.terminate_instances(instance_ids: instance_ids)
+
+        loop do
+          instances = ec2_client.describe_instances(instance_ids: instance_ids).reservations.flat_map(&:instances)
+          break if instances.all? {|instance| instance.state.name == "terminated" }
+
+          @logger&.info("Waiting for stopping all instances...")
+          instances.sort_by(&:instance_id).each do |instance|
+            @logger&.info("#{instance.instance_id}\t#{instance.state.name}")
+          end
+          sleep 10
+        end
+      end
+    end
+  end
+end
---
+++ spec/ecs_deploy/auto_scaler/instance_fluctuation_manager_spec.rb
@@ -0,0 +1,168 @@
+require "spec_helper"
+
+require "logger"
+require "stringio"
+require "ecs_deploy/auto_scaler/instance_fluctuation_manager"
+
+RSpec.describe EcsDeploy::AutoScaler::InstanceFluctuationManager do
+  let(:logdev) do
+    StringIO.new
+  end
+  let(:instance_fluctuation_manager) do
+    described_class.new(
+      region: "ap-northeast-1",
+      cluster: "cluster",
+      cluster_to_asg: { "cluster" => "asg-cluster", "cluster2" => "asg-cluster2" },
+      desired_capacity: 50,
+      logger: ::Logger.new(logdev)
+    )
+  end
+
+  describe "#increase" do
+    context "w/o error" do
+      before do
+        @auto_scaling_groups = [
+          Aws::AutoScaling::Types::AutoScalingGroup.new(
+            desired_capacity: 50,
+            max_size: 100
+          )
+        ]
+        Aws.config[:autoscaling] = {
+          stub_responses: {
+            describe_auto_scaling_groups: lambda do |_|
+              Aws::AutoScaling::Types::AutoScalingGroupsType.new(
+                auto_scaling_groups: @auto_scaling_groups,
+              )
+            end,
+            update_auto_scaling_group: lambda do |_|
+              # no error
+              nil
+            end
+          }
+        }
+
+        cluster = Aws::ECS::Types::Cluster.new(registered_container_instances_count: 50)
+        expect(cluster).to receive(:registered_container_instances_count)
+          .exactly(5).times.and_return(60, 70, 80, 90, 100)
+        @clusters = [cluster]
+        Aws.config[:ecs] = {
+          stub_responses: {
+            describe_clusters: lambda do |_|
+              Aws::ECS::Types::DescribeClustersResponse.new(clusters: @clusters)
+            end
+          }
+        }
+
+        allow(instance_fluctuation_manager).to receive(:sleep)
+      end
+
+      it "succeeded to increase instances" do
+        thread = instance_fluctuation_manager.increase
+        thread.join
+        log = logdev.string
+        expect(log).to include("Increase desired capacity of asg-cluster: 50 => 100")
+        [60, 70, 80, 90].each do |count|
+          expect(log).to include("Current registered instance count: #{count}")
+        end
+        expect(log).to include("Succeeded to increase instance count!")
+      end
+    end
+  end
+
+  describe("#decrease") do
+    context "w/o error" do
+      before do
+        @auto_scaling_groups = [
+          Aws::AutoScaling::Types::AutoScalingGroup.new(
+            desired_capacity: 100,
+            max_size: 100
+          )
+        ]
+        Aws.config[:autoscaling] = {
+          stub_responses: {
+            describe_auto_scaling_groups: lambda do |_|
+              Aws::AutoScaling::Types::AutoScalingGroupsType.new(
+                auto_scaling_groups: @auto_scaling_groups,
+              )
+            end,
+            update_auto_scaling_group: lambda do |_|
+              # no error
+            end,
+            detach_instances: lambda do |_|
+              # no error
+            end
+          }
+        }
+
+        arns = (1..100).to_a.map {|n| sprintf("arn:aws:ecs:ap-northeast-1:xxx:container-instance/%03d", n) }
+        availability_zones = [
+          Aws::ECS::Types::Attribute.new(name: "ecs.availability-zone", value: "zone-1"),
+          Aws::ECS::Types::Attribute.new(name: "ecs.availability-zone", value: "zone-2"),
+          Aws::ECS::Types::Attribute.new(name: "ecs.availability-zone", value: "zone-3"),
+          Aws::ECS::Types::Attribute.new(name: "ecs.availability-zone", value: "zone-4")
+        ]
+        container_instances = arns.map do |arn|
+          Aws::ECS::Types::ContainerInstance.new(
+            container_instance_arn: arn,
+            running_tasks_count: rand(1..10),
+            attributes: [availability_zones.sample],
+            ec2_instance_id: "ec2-#{arn}"
+          )
+        end
+        task_arns = (1..10).to_a.map {|n| sprintf("task-arn%02d", n) }
+        tasks = task_arns.map do |arn|
+          group = ["family:#{arn}", "dummy:#{arn}"].sample
+          Aws::ECS::Types::Task.new(task_arn: arn, group: group)
+        end
+        Aws.config[:ecs] = {
+          stub_responses: {
+            list_container_instances: lambda do |_|
+              Aws::ECS::Types::ListContainerInstancesResponse.new(container_instance_arns: arns)
+            end,
+            describe_container_instances: lambda do |_|
+              Aws::ECS::Types::DescribeContainerInstancesResponse.new(container_instances: container_instances)
+            end,
+            update_container_instances_state: lambda do |_|
+              # no error
+            end,
+            list_tasks: lambda do |_|
+              Aws::ECS::Types::ListTasksResponse.new(task_arns: task_arns)
+            end,
+            describe_tasks: lambda do |_|
+              Aws::ECS::Types::DescribeTasksResponse.new(tasks: tasks)
+            end,
+            stop_task: lambda do |_|
+              # no error
+            end
+          }
+        }
+        # Must stub after set :stub_responses to Aws.config[:ecs]
+        ecs_client = instance_fluctuation_manager.send(:ecs_client)
+        allow(ecs_client).to receive(:wait_until)
+        expect(ecs_client).to receive(:stop_task).at_most(arns.size * tasks.size).times
+
+        state_terminated = Aws::EC2::Types::InstanceState.new(code: 48, name: "terminated")
+        Aws.config[:ec2] = {
+          stub_responses: {
+            describe_instances: lambda do |_|
+              Aws::EC2::Types::DescribeInstancesResult.new(
+                reservations: [
+                  Aws::EC2::Types::Reservation.new(instances: [Aws::EC2::Types::Instance.new(state: state_terminated, instance_id: "instance_id")])
+                ]
+              )
+            end
+          }
+        }
+
+        allow(instance_fluctuation_manager).to receive(:sleep)
+      end
+
+      it "succeeded to decrease instances" do
+        instance_fluctuation_manager.decrease
+        log = logdev.string
+        expect(log).to include("Decrease desired capacity of asg-cluster: 100 => 50")
+        expect(log).to include("Succeeded to decrease instances!")
+      end
+    end
+  end
+end
