20671cc9544ecd1d35dbb9dc2cde225f781ee2c7
abicky <takeshi.arabiki@gmail.com>
2023-02-03 12:20:31 +0900

    Make code in describe_detachable_instances simpler

--- lib/ecs_deploy/auto_scaler/auto_scaling_group_config.rb
+++ lib/ecs_deploy/auto_scaler/auto_scaling_group_config.rb
@@ -181,7 +181,7 @@ module EcsDeploy
         client.describe_auto_scaling_groups({ auto_scaling_group_names: [name] }).auto_scaling_groups[0].instances.reject do |i|
           # The lifecycle state of terminated instances becomes "Detaching", "Terminating", "Terminating:Wait", or "Terminating:Proceed",
           # and we can't detach instances in such a state.
-          next true if i.lifecycle_state.start_with?("Terminating") || i.lifecycle_state == "Detaching"
+          i.lifecycle_state.start_with?("Terminating") || i.lifecycle_state == "Detaching"
         end
       end
 
